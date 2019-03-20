260621440474f9a9321c21b7326a6fb6d4e8fefe
Hao Qian <hqian5@sheffield.ac.uk>
2019-03-20 14:25:00 +0000

    Redesign of index and login pages

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,2 +1,2 @@
-checkCookie();
+
 
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -31,9 +31,7 @@ function sendAjaxQuery(url, data) {
             else if (JSON.stringify(ret.code) == 0){
                 nameTip.innerText ='';
                 passwordTip.innerText = '';
-                setCookie("username", userName, 1);
                 alert(userName + ", welcome to Show-Off~");
-                console.log(getCookie("username"));
                 window.location.href = '/index';
             }
             else if(JSON.stringify(ret.code) == 1){
@@ -60,37 +58,4 @@ function onSubmit() {
     sendAjaxQuery('/api/login', data);
     event.preventDefault();
 
-}
-
-function setCookie(cname, cvalue, exhours) {
-
-    var d = new Date();
-    d.setTime(d.getTime() + (exhours*60*1000));
-    var expires = "expires" + d.toTimeString();
-    document.cookie = cname + "=" + cvalue + ";" + expires;
-
-}
-
-function getCookie(cname)
-{
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++)
-    {
-        var c = ca[i].trim();
-        if (c.indexOf(name) == 0)
-            return c.substring(name.length,c.length);
-    }
-    return "";
-
-}
-
-function checkCookie()
-{
-    var username = getCookie("username");
-    if (username == 'abc'){
-        setTimeout( function noDisplay() {
-            document.getElementById("buttons").style.display="none";
-        }, 10)
-    }
 }
